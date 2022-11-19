f9c19055378268f4f6d96db929983f95c20f1c49
Manama_abovetrans_gcloudshell <prykryk@gmail.com>
2022-11-19 16:07:15 +0000

    Test commit abovetrans cloud 1

--- install_xfce_google_RDP.sh
+++ install_xfce_google_RDP.sh
@@ -4,25 +4,31 @@ sudo aptitude install -y xscreensaver
 sudo apt install dbus-x11 -y
 sudo apt-get install gconf-service -y
 sudo apt-get update
-sudo add-apt-repository ppa:saiarcot895/chromium-beta
+#sudo add-apt-repository ppa:saiarcot895/chromium-beta
 #sudo apt-get install chromium-browser -y
 sudo apt install firefox-esr -y
 
 #sudo apt install chromium - y
 
 #Install Google remote desktop
-cd Download
+if [[ ! -f "chrome-remote-desktop_current_amd64.deb" ]] ;
+#cd Download
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+fi
 sudo dpkg -i chrome-remote-desktop_current_amd64.deb 
 # Check and fix this one via https://remotedesktop.google.com/headless
 #DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AfgeXvsfP11b9rVHZFeMR9daeFSA0ZbQ2kYmzcXxtrbFwnDEQXUzfr1E7dEZy1NS-IeK2g" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)
 echo Go to  https://remotedesktop.google.com/access/session/49e2ce50-33e2-4ed7-b528-6400000311f7 now and download command
 
 #install teamviewer 
-cd Download 
+#cd Download 
+if [[ ! -f "teamviewer-host_amd64.deb" ]] ;
 wget https://download.teamviewer.com/download/linux/teamviewer-host_amd64.deb
+fi
 sudo dpkg -i teamviewer-host_amd64.deb
 apt --fix-broken install
+sudo aptitude install teamviewer-host -y
+
 bash ~/demonize_system_snap.sh 
 sudo teamviewer daemon start   
 teamviewer setup
