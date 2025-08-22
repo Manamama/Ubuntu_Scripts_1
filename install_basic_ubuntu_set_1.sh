7324ccaf0a7124485d38897514e29808cb95ce8b
Ma Mana ma Manama <78492008+Manamama@users.noreply.github.com>
2025-08-22 19:59:10 +0200

    Update install_basic_ubuntu_set_1.sh

--- install_basic_ubuntu_set_1.sh
+++ install_basic_ubuntu_set_1.sh
@@ -142,6 +142,8 @@ curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/ke
 echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
 sudo apt update && sudo apt install glow
 
+npm install -g rust-just
+
 }
 
 # --- Modern CMake ---
