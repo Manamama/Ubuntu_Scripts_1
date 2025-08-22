65a730876e085acf7737243966c6bfaccb7d8cd6
Ma Mana ma Manama <78492008+Manamama@users.noreply.github.com>
2025-08-22 18:49:08 +0200

    Update install_basic_ubuntu_set_1.sh

--- install_basic_ubuntu_set_1.sh
+++ install_basic_ubuntu_set_1.sh
@@ -93,7 +93,7 @@ install_system_tools() {
 
     # Core dev tools
     sudo apt-get install -y pciutils build-essential cmake curl libcurl4-openssl-dev \
-        libomp-dev libssl-dev adb fastboot neofetch geoip-bin ranger baobab firefox python3-pip
+        libomp-dev libssl-dev adb fastboot neofetch geoip-bin ranger baobab firefox python3-pip npm 
 
     # Optional: cpufetch
     if apt-cache show cpufetch >/dev/null 2>&1; then
