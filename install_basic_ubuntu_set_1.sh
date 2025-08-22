cea91f127bf310102823e81c37951cdbf10697a3
Ma Mana ma Manama <78492008+Manamama@users.noreply.github.com>
2025-08-22 19:04:54 +0200

    Update install_basic_ubuntu_set_1.sh

--- install_basic_ubuntu_set_1.sh
+++ install_basic_ubuntu_set_1.sh
@@ -134,6 +134,12 @@ install_system_tools() {
     wget -c https://dl.google.com/android/repository/platform-tools-latest-linux.zip
     unzip -o platform-tools-latest-linux.zip
     sudo cp -r platform-tools/* /usr/bin/
+    # Debian/Ubuntu glow
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
+echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
+sudo apt update && sudo apt install glow
+
 }
 
 # --- Modern CMake ---
