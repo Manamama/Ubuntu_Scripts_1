0c3114170544bc0e1cee8ced1a1203dd67187233
Ma Mana ma Manama <78492008+Manamama@users.noreply.github.com>
2025-08-25 23:22:33 +0200

    Update bashrc-gcloud.sh

--- bashrc-gcloud.sh
+++ bashrc-gcloud.sh
@@ -140,8 +140,9 @@ source /google/devshell/bashrc.google
 export PATH="$PATH:$HOME/.local/usr/bin"
 mkdir -p "$HOME/.local/var/lib/dpkg"
 
-mkdir -p /home/abovetrans/.local/var/lib/dpkg
+#mkdir -p $HOME/.local/var/lib/dpkg
 
+# git clone https://github.com/Manamama/Ubuntu_Scripts_1/tree/main 
 # And install there for permanence: sudo dpkg --instdir=/home/abovetrans/.local --admindir=/home/abovetrans/.local/var/lib/dpkg --no-triggers -i gotop_v4.2.0_linux_amd64.deb
 
 #You can bind /home, but not advised, as it is a temp, ephemeral folder:
